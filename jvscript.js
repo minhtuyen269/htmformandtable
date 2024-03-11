eead597d3787414a466a2d6b724596aca6fa9f3b
minhtuyen248 <minhtuyen269@gmail.com>
2024-03-11 08:24:06 +0700

    Hoan Thanh

---
+++ jvscript.js
@@ -0,0 +1,36 @@
+//alert("Hoan Thanh1")
+
+//let a= 1;
+//let b= 2;
+//let sum=a+b;
+//alert(sum)
+/*function addata() {
+    let b = document.getElementById("a").value
+    alert(b)
+}*/
+
+
+//Bài 1: Cho a = 10, b = 5, c = 20, d= 15.
+// Tính và hiển thị
+// (console.log, alert, document.write) a + b * c - d / 2.
+
+    let a = prompt('ImportNumber1')
+    let b = prompt('ImportNumber2')
+    let c = prompt('ImportNumber3')
+    let d = prompt('ImportNumber4')
+
+    a= parseInt(a)
+    b= parseInt(b)
+    c= parseInt(c)
+    d= parseInt(d)
+
+    /*  let sum = a + b;
+    let sub = a - b;
+    let core = a * b;
+    let div = a / b;           */
+     let sum = a + b * c - d / 2
+
+    alert(sum)
+    /*  alert(sub)
+    alert(core)
+    alert(div)  */
